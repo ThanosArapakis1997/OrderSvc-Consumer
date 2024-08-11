3a9b410d06ee1592bae4d300b9a020292160504a
unknown <michaelzeakis13@gmail.com>
2024-08-11 15:23:09 +0300

    provider service and tests

---
+++ DiscountSvc-Provider/Services/DiscountService.cs
@@ -0,0 +1,12 @@
+﻿namespace DiscountSvc_Provider.Services
+{
+    public class DiscountService
+    {
+        public double GetDiscountAmount(double customerRating)
+        {
+            return customerRating / 100;
+        }
+
+
+    }
+}
---
+++ ProviderTesting/XUnitOutput.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestPlatform.Utilities;
+using Xunit.Abstractions;
+using IOutput = PactNet.Infrastructure.Outputters.IOutput;
+
+namespace ProviderTesting
+{
+    public class XUnitOutput : IOutput
+    {
+        private readonly ITestOutputHelper _output;
+        public XUnitOutput(ITestOutputHelper output)
+        {
+            _output = output;
+        }
+        public void Write(string message, OutputLevel level)
+        {
+            _output.WriteLine(message);
+        }
+        public void WriteLine(string line)
+        {
+            _output.WriteLine(line);
+        }
+        public void WriteLine(string message, OutputLevel level)
+        {
+            _output.WriteLine(message);
+        }
+    }
+}
